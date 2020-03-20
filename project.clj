d591fc121f2540afc52392274d3785aba521b6a1
Naoki OGAWA <na-ogawa@aruka.sakura.ne.jp>
2020-03-20 17:27:17 +0900

    enable both uberjar and repl

--- project.clj
+++ project.clj
@@ -3,9 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :repl-options {:init-ns gobgp-grpc-java.core}
   :main ^skip-aot gobgpapi.example.Client
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
+
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [javax.annotation/javax.annotation-api "1.3.2"]
                  [com.google.protobuf/protobuf-java "3.11.0"]
@@ -27,6 +29,5 @@
   :java-source-paths ["src/main/java" "target/generated-sources/protobuf"]
   :javac-options {:debug "true"}
 
-  ;:aot [gobgpapi.example.ListPath]
-
-  :repl-options {:init-ns gobgp-grpc-java.core})
+  :jar-exclusions [#".java$" #"^main/" #"^META-INF/maven/" #"^clojure/" ]
+)
