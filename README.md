98e02a38e060c9ff3a35a3117ceb45f5fac950ad
Naoki Ogawa <n-ogawa@sakura.ad.jp>
2020-03-23 10:56:55 +0900

    update README.md, and some

--- README.md
+++ README.md
@@ -9,5 +9,16 @@
 - Now instead of maven, I start to use [leiningen](https://leiningen.org/) as build tool.
 - `lein uberjar` then being run protoc, javac and uberjar.      
 - `java -jar target/uberjar/gobgp-grpc-java-0.1.0-SNAPSHOT-standalone.jar` then a example will run.
-- `lein repl` can let me feel like interpreter.
+- `lein repl` can let me feel like running interpreter, strictly not.
+
+```Clojure
+gobgp-grpc-java.core=> (gobgpapi.example.Client/process "add")
+call addPath
+Done
+nil
+gobgp-grpc-java.core=> (gobgpapi.example.Client/process "del")
+call delPath
+Done
+nil
+```
 
--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject gobgp-grpc-java "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "call gobgp-api from java"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :repl-options {:init-ns gobgp-grpc-java.core}
@@ -10,16 +9,21 @@
 
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [javax.annotation/javax.annotation-api "1.3.2"]
-                 [com.google.protobuf/protobuf-java "3.11.0"]
+                 [com.google.protobuf/protobuf-java "3.11.4"]
                  [io.grpc/grpc-api "1.28.0"]
                  [io.grpc/grpc-core "1.28.0" :exclusions [io.grpc/grpc-api]]
                  [io.grpc/grpc-netty-shaded "1.28.0" :exclusions [io.grpc/grpc-api io.grpc/grpc-core]]
                  [io.grpc/grpc-protobuf "1.28.0"]
                  [io.grpc/grpc-stub "1.28.0"]]
 
-  :plugins [[lein-protoc "0.5.0"]]
+  :plugins [[lein-protoc "0.5.0"]
+            [lein-cljfmt "0.6.7"]
+            [jonase/eastwood "0.3.10"]
+            [lein-kibit "0.1.8"]
+           ]
+
   ; for lein-protoc
-  :protoc-version "3.11.0"
+  :protoc-version "3.11.4"
   :proto-source-paths ["src/main/proto"]
   :protoc-grpc {:version "1.28.0"}
   :proto-target-path "target/generated-sources/protobuf"
@@ -29,5 +33,6 @@
   :java-source-paths ["src/main/java" "target/generated-sources/protobuf"]
   :javac-options {:debug "true"}
 
+  ; for uberjar
   :jar-exclusions [#".java$" #"^main/" #"^META-INF/maven/" #"^clojure/" ]
 )
